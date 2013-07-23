0f9a336fa724cb8637989a06e43f024efc657d51
ctres <treseler@gmail.com>
2013-07-23 10:37:53 +0200

    fixed javascript issue.
    users can logout again.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Blog::Application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
-                     controllers: {omniauth_callbacks: "omniauth_callbacks"}
+                     controllers: {omniauth_callbacks: "omniauth_callbacks"},
+                     :sign_out_via => [ :post, :delete ]
   root to: 'features#index'
-  resources :users
+  
+  #resources :users
+  resources :collaborators
   resources :features do
   	get :autocomplete_tag_name, :on => :collection
   end
   
-  resources :collaborators
 end
