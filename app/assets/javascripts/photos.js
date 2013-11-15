113560399d6013a31189bd0b790e6255c6067ac4
Colin Treseler <treseler@gmail.com>
2013-11-15 23:03:53 +0100

    commited.

---
+++ app/assets/javascripts/photos.js
@@ -0,0 +1,13 @@
+$(document).on('mouseenter', '.image', function () { 
+  var img = $('img', this);
+    $(this).append('class="mediumImages-remove"');
+    var positionTop = img.position().top + parseInt(img.css('margin-top'), 10);
+    alert(positionTop);
+    var positionLeft = img.position().left + img.width() -30;
+    $('.mediumImages-remove', this).css({
+      'right': 'auto',
+      'top': positionTop,
+      'left': positionLeft
+    });
+
+});
